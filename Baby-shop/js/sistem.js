c177278bec6d31baf9704959688c3784e9ab3a39
unknown <Everon1988@maiil.ru>
2019-02-20 20:38:27 +0300

    Увеличил шрифты для моб версии. Кнопка закрытия меню

--- Baby-shop/js/sistem.js
+++ Baby-shop/js/sistem.js
@@ -1,9 +1,19 @@
-var openMenuButton = document.querySelector('.main-navigation__button-wrapper');
+var openMenuButton = document.querySelector('.main-navigation__open-button');
+var closeMenuButton = document.querySelector('.main-navigation__close-button')
 var menu = document.querySelector('.main-navigation__category-list');
+var search = document.querySelector('.search-form__button');
 
 var openMenu = function(evt) {
 	menu.classList.remove('visually-hidden');
 	openMenuButton.classList.add('hidden');
+	search.classList.add('hidden');
 };
 
+var closeMenu = function() {
+	menu.classList.add('visually-hidden');
+	openMenuButton.classList.remove('hidden');
+	search.classList.remove('hidden');
+}
+
 openMenuButton.addEventListener('click', openMenu);
+closeMenuButton.addEventListener('click', closeMenu)
