151bac41dac1e928b20340d6fe987f5e378f2e79
unknown <Everon1988@maiil.ru>
2019-02-20 14:51:33 +0300

    Шапка почти готова

---
+++ Baby-shop/js/sistem.js
@@ -0,0 +1,4 @@
+/*var logo = document.querySelector('.logo');
+
+logo.style.left = document.documentElement.clientWidth / 2 - logo.clientWidth + 'px'
+console.dir(document.documentElement.clientWidth);*/
